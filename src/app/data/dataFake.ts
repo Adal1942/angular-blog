c8d7fecf27452ac7cc7983b938444c2eebc9265a
Adal1942 <adautosombra1942@gmail.com>
2023-05-28 20:24:31 -0300

    add contents

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,30 @@
+export const dataFake = [
+    {
+        "id":"1",
+        "photo": "aaa",
+        "title": "Leah",
+        "description": "Uma amante da natureza, Leah tem prazer em viver próxima a natureza e ter uma refeição mais caseira e selvagem. Ela também é artista e faz esculturas, mas está insegura sobre mostrar seu trabalho para o mundo e pode precisar de um empurrãozinho de alguém que ela confia. Ela está tão na sua cara! Muitas vezes, ela pode ser encontrada na Floresta Cindersap apreciando o tempo junto ao lago, também pode ser encontrada na praia no verão ou no Bar Stardrop à noite. Ela vive em um pequeno chalé, ao sul do Rancho da Marnie e está inacessível até que você chegue a 2 corações com ela. Normalmente, ela não deixa seu chalé antes das 10:00 AM.",
+        "photoCover": "../../assets/img/Leah.png"
+    },
+    {
+        "id":"2",
+        "photo": "aaa",
+        "title": "Harvey",
+        "description": "Harvey é o médico da cidade . Ele é um pouco velho para um solteiro , mas ele tem um coração amável e uma posição respeitada na comunidade . Ele vive em um pequeno apartamento acima da clínica médica , mas passa a maior parte de seu trabalho a tempo parcial . Você pode sentir uma tristeza sobre ele, como se houvesse algo que ele não está dizendo a você...",
+        "photoCover": "../../assets/img/Harvey.png"
+    },
+    {
+        "id":"3",
+        "photo": "aaa",
+        "title": "Abigail",
+        "description": "Abigail vive em uma loja com seus pais.As vezes ela briga com sua mãe, que se preocupa com seu jeito de viver.Ela está sempre dizendo: eu queria que a Abby usa-se roupas mais apropriadas e para-se de pintar o cabelo de roxo, ela tem uma cor natural de cabelo maravilhosa assim como o da sua vó, oh e eu queria que ela tive-se interesses saudáveis em vez dessas coisas absurdas que ela está se metendo.",
+        "photoCover": "../../assets/img/Abigail.png"
+    },
+    {
+        "id":"4",
+        "photo": "aaa",
+        "title": "Sebastian",
+        "description": "Sebastian é um solitário rebelde que vive no porão da casa de seus pais . Ele é o meio-irmão mais velho de Maru, e acha que sua irmã recebe toda a atenção e adoração , enquanto ele é deixado para apodrecer no escuro. Ele tende a ficar profundamente viciado em jogos de computador , revistas em quadrinhos e romances de ficção científica , e às vezes ele passa um bom tempo fazendo seus hobbies trancado no seu quarto.Ele pode ser um pouco hostil para as pessoas que ele não conhece . Poderia um charmoso novo agricultor cultivar o terreno baldio do seu coração? Quem sabe?",
+        "photoCover": "../../assets/img/Sebastian.png"
+    },
+]
